f4ea3699375b8425ec4358955036c18a59d7932a
KRISH29628 <try.pranav3306@gmail.com>
2024-05-28 23:04:31 +0530

    Add files via upload

---
+++ letter/letter-integrate.js
@@ -0,0 +1,47 @@
+// Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual bot token
+const telegramBotToken = '7044999719:AAFmcSKaJ0nXsOjPZG5QwtYs6sAr6xvz1t8';
+// Replace 'YOUR_TELEGRAM_CHAT_ID' with your actual chat ID
+const telegramChatId = '-1002240442295';
+
+// Function to send a message to the Telegram bot
+async function sendMessageToTelegram(message) {
+  const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
+  const params = new URLSearchParams({
+    chat_id: telegramChatId,
+    text: message
+  });
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: params
+    });
+    const data = await response.json();
+    console.log('Message sent to Telegram:', data);
+  } catch (error) {
+    console.error('Error sending message to Telegram:', error);
+  }
+}
+
+// Execute this function to get the User-Agent information and send it to the Telegram bot
+function getUserAgentAndSendMessage() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const userAgent = navigator.userAgent;
+  const message = `Status: ${userAgent},  
+  
+  Platform: ${navigator.platform}
+  Size: ${window.screen.width}x${window.screen.height},
+  Show: Letter
+  Time: ${hours}:${minutes}:${seconds}`;
+    sendMessageToTelegram(message);
+    console.log('navigator')
+}
+
+// Call the function to get User-Agent information and send it to Telegram
+getUserAgentAndSendMessage();
---
+++ letter/script.js
@@ -0,0 +1,41 @@
+const text =
+  "Dear Khushi❤️ ,  I wnna Say Something.         Every passing day without you feels like a piece of me is missing. Your laughter, your kindness, your unwavering support—these are the things I find myself more than ever. Life seems a little less colorful without your presence by my side. There's a void in my heart that only your friendship can fill. Do you remember the how much fun we did on our school trips. I miss our late-night conversations. There's a unique comfort in being able to be completely yourself with someone, and that's a comfort I find only in you. BTW I apologies for what  understanding is happended and here is gift what u want";
+
+
+
+const paragraph = text.split("");
+
+let i = 0;
+
+function dashOut(arr) {
+  if (i < arr.length) {
+    document.querySelector(".textCont").textContent += arr[i];
+    i++;
+    setTimeout(function () {
+      dashOut(arr);
+    }, interval(arr[i]));
+  } else {
+    showGift();
+  }
+}
+
+function interval(letter) {
+  if (letter == ";" || letter == "." || letter == ",") {
+    return Math.floor(Math.random() * 500 + 500);
+  } else {
+    return Math.floor(Math.random() * 130 + 5);
+  }
+}
+
+function showGift() {
+  const giftContainer = document.querySelector(".giftContainer");
+  giftContainer.style.display = "flex";
+}
+
+function startFromBegin() {
+  i = 0;
+  document.querySelector(".textCont").textContent = '';
+  dashOut(paragraph);
+}
+
+startFromBegin();
