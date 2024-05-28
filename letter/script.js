a9888b402a9fdc6c64c37be0350b7dc3e95b6a92
KRISH29628 <try.pranav3306@gmail.com>
2024-05-28 23:09:41 +0530

    Update script.js

--- letter/script.js
+++ letter/script.js
@@ -1,5 +1,5 @@
 const text =
-  "Dear Khushi❤️ ,  I wnna Say Something.         Every passing day without you feels like a piece of me is missing. Your laughter, your kindness, your unwavering support—these are the things I find myself more than ever. Life seems a little less colorful without your presence by my side. There's a void in my heart that only your friendship can fill. Do you remember the how much fun we did on our school trips. I miss our late-night conversations. There's a unique comfort in being able to be completely yourself with someone, and that's a comfort I find only in you. BTW I apologies for what  understanding is happended and here is gift what u want";
+  "Dear Khushi❤️ ,  I wnna Say Something.         Every passing day without you feels like a piece of me is missing. Your laughter, your kindness, your unwavering support—these are the things I find myself more than ever. Life seems a little less colorful without your presence by my side. There's a void in my heart that only your friendship can fill. Do you remember the how much fun we did on our school trips. I miss our late-night conversations. There's a unique comfort in being able to be completely yourself with someone, and that's a comfort I find only in you. BTW I apologies for what misunderstanding is happended and here is gift what u want";
 
 
 
